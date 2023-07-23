dac9e17ed7ef9005b00e1280b3c37de1c0f11dfe
geunskoo <oryukdo3@pusan.ac.kr>
2023-07-23 23:08:27 +0900

    section3: 문자열 보간, 속성 바인딩, 이벤트 바인딩, 양방향 바인딩을 이용한 data communication 전략

--- angular-basic/src/app/servers/servers.component.ts
+++ angular-basic/src/app/servers/servers.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>
-  `,
+  templateUrl: './servers.component.html',
     styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
 
+
+  allowNewServer:boolean = false;
+  serverCreationStatus:string = 'No server was Created!';
+  serverName = 'TestServer';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;  
+    },2000);
+
+  }
+
+  ngOnInit(): void {
+  
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was Created! Name is ' + this.serverName;
+  }
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
