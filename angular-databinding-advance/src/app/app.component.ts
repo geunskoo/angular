6b5cb80a953297a67932327aed26bf879548be98
Tae Geun Kim <oryukdo3@pusan.ac.kr>
2023-07-24 10:17:46 +0900

    section5: databinding 심화 학습 파일 재생성

--- angular-databinding-advance/src/app/app.component.ts
+++ angular-databinding-advance/src/app/app.component.ts
@@ -11,18 +11,10 @@ export class AppComponent {
   newServerContent = '';
 
   onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
+
   }
 
   onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
+
   }
 }
