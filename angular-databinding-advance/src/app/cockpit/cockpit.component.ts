d7e6f1bc499becc0066d13d2366823b5ecc6f579
Tae Geun Kim <oryukdo3@pusan.ac.kr>
2023-07-24 14:28:35 +0900

    section5: databinding 학습 데이터 추가

--- angular-databinding-advance/src/app/cockpit/cockpit.component.ts
+++ angular-databinding-advance/src/app/cockpit/cockpit.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
   styleUrls: ['./cockpit.component.css']
 })
-export class CockpitComponent {
+export class CockpitComponent implements OnInit{
+
+  @Output('srvCreated') serverCreated = new EventEmitter<{serverName: string, serverContent: string}>(); //이벤트 방출자
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>(); //이벤트 방출자;
+  newServerName = '';
+  newServerContent = '';
+
+  constructor() {
+    
+  }
+  ngOnInit() {
+
+  }
+
+  onAddServer(){
+    this.serverCreated.emit({
+      serverName: this.newServerName, serverContent: this.newServerContent
+    });
+  }
+
+  onAddBlueprint(){
+    this.blueprintCreated.emit({
+      serverName: this.newServerName, serverContent: this.newServerContent
+    });
+  }
 
 }
