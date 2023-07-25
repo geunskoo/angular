5a53a22b76fd832cb94d13dc4b3f653c0f6678a9
Tae Geun Kim <oryukdo3@pusan.ac.kr>
2023-07-25 17:51:57 +0900

    section7: 메세지 로직, 데이터 접근 서비스 추가하기

---
+++ angular-heros/src/app/hero.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROS } from './mock-heros';
+import { Observable, of } from 'rxjs';
+import { MessagesService } from './messages.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService: MessagesService) { }
+
+  getHeroes(): Observable<Hero[]>{
+    const heros = of(HEROS);
+    this.messageService.add('HeroService: fetched heros');
+    return heros;
+  }
+
+}
--- angular-heros/src/app/heroes/heroes.component.ts
+++ angular-heros/src/app/heroes/heroes.component.ts
@@ -1,19 +1,41 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { Observable, of } from 'rxjs';
 import { HEROS } from '../mock-heros';
+import { MessagesService } from '../messages.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit{
 
-  heros: Hero[] = HEROS;
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
 
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessagesService
+  ){}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add( `HerosComponent : Selected hero id=${hero.id}` );
+  }
+
+  // getHeros(): void{
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+
+  getHeroes(): void{
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
