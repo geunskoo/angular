90887e6cd76b7ff69f898809bf9f7df5ab419bd0
Tae Geun Kim <oryukdo3@pusan.ac.kr>
2023-07-30 12:53:27 +0900

    section8: 공부노트 예제를 만들기 위해 page 수정 및 분리

---
+++ angular-ionic/src/app/page-models/app-pages.ts
@@ -0,0 +1,17 @@
+export interface Page{
+    title: string,
+    url: string,
+    icon: string
+}
+
+export const appPages: Page[] = [
+    { title: 'spring', url: '/folder/spring', icon: 'heart' },
+    { title: 'java', url: '/folder/java', icon: 'heart' },
+    { title: 'hexagonal Architecture', url: '/folder/hexagonal', icon: 'heart' },
+    { title: 'angular', url: '/folder/angular', icon: 'heart' },
+    { title: 'ionic', url: '/folder/ionic', icon: 'heart' },
+  ];
+
+  export const labels: string[] = [
+    'made by geunskoo'
+];
